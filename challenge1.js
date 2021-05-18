54665d364cd409525c1e489bbc9ee13ad5405020
Glitch (quirky-childlike-pull) <none>
2021-05-18 20:07:52 +0000

    👱🐁 Checkpoint ./template1.html:3856783/17 ./challenge1.js:3856783/267

--- challenge1.js
+++ challenge1.js
@@ -4,5 +4,18 @@ function setup() {
 
 function draw() {
   background(230);
+  
+  fill(250, 250, 0)
+  ellipse(0, 0, 55, 55);
+  
+  fill(0, 250, 250)
+  ellipse(600, 0, 55, 55);
+  
+  fill(250, 0, 250)
+  ellipse(0, 500, 55, 55);
+  
+  fill(0, 250, 0)
+  ellipse(500, 600, 55, 55);
+  
   // ellipse(20, 20, 20)
 }
