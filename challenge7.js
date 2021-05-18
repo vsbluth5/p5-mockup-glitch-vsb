0632215bcef5f5bda7fa5e2786ee7eeb332d3782
Glitch (quirky-childlike-pull) <none>
2021-05-18 20:31:57 +0000

    🏎🚶 Checkpoint ./template7.html:3856783/17 ./challenge7.js:3856783/165

--- challenge7.js
+++ challenge7.js
@@ -4,9 +4,13 @@ function setup() {
 
 function draw() {
   background(230);
+  stroke(255, 204, 0);
+strokeWeight(8);
+  fill(55, 55, 150);
+  
   beginShape(TRIANGLE_FAN);
-  vertex(57.5, 50);
-  vertex(57.5, 15);
+  vertex(300, 450);
+  vertex(300, 20);
   vertex(92, 50);
   vertex(57.5, 85);
   vertex(22, 50);
