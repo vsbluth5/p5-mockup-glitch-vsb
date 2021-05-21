73dd79b8c3ac7008d67760d058d0a3f81ceeda39
Glitch (quirky-childlike-pull) <none>
2021-05-21 02:42:14 +0000

    🚔👶 Checkpoint ./challenge7.js:3856783/448

--- challenge7.js
+++ challenge7.js
@@ -10,7 +10,19 @@ strokeWeight(8);
   
   // What is TRIANGLE_FAN?
   // This is definitely not the best way to draw a star
-  beginShape(TRIANGLE_FAN);
+  
+// The parameters available for beginShape() 
+  // are POINTS, LINES, TRIANGLES, TRIANGLE_FAN, 
+  // TRIANGLE_STRIP, QUADS, QUAD_STRIP, 
+  // and TESS (WebGL only). 
+  // After calling the beginShape() function, 
+  // a series of vertex() commands must follow. 
+  // To stop drawing the shape, call endShape(). 
+  // Each shape will be outlined with 
+  // the current stroke color and filled 
+  // with the fill color.
+  
+  beginShape(LINES);
   
   vertex(300, 20);
   vertex(150, 450);
